c49751766fc199ad7a834cff44f6e707cfe9c442
Harvey Javier <harveyjavier17@gmail.com>
2025-01-12 15:22:26 +0800

    Improve About page spacing and make Team section less scrollable on desktop view

--- components/ui/about.tsx
+++ components/ui/about.tsx
@@ -4,14 +4,14 @@ import Image from 'next/image'
 export function About() {
   return (
     <section id="about-us" className="container mx-auto px-6 md:px-16 py-12 md:py-20">
-      <div className="grid gap-6 place-content-center">
+      <div className="grid gap-4 place-content-stretch">
         <h1 className="text-4xl">
           <span className="font-bold">SparkPoint.</span> Reignited. Focused. Powered By AI.
         </h1>
-        <div className='flex gap-8 md:flex-row flex-col items-center justify-center'>
-          <div className='grid gap-8'>
+        <div className='flex gap-8 md:flex-row flex-col items-center justify-between'>
+          <div className='grid gap-8 md:w-[60%]'>
             <div className="grid text-left gap-3">
-              <p className="text-2xl">
+              <p className="text-2xl w-full">
                 Founded in 2018, SparkPoint Technologies Inc is
                 reimagining blockchain for the AI era. We empower
                 individuals and businesses to launch AI-driven, blockchain-based solutions
@@ -25,7 +25,7 @@ export function About() {
               innovation and community-driven growth.
             </p>
           </div>
-          <Image src={new_sparkpoint_logo_w_text} alt="SparkPoint Logo" className="w-full h-auto md:w-[60%]" />
+          <Image src={new_sparkpoint_logo_w_text} alt="SparkPoint Logo" className="w-3/4 h-auto md:w-[25%]" />
         </div>
       </div>
     </section>
