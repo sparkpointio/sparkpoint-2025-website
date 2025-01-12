1b34a041f64b1bbd0cd9206d1e056ecf5a97bb28
Harvey Javier <harveyjavier17@gmail.com>
2025-01-12 09:31:22 +0800

    Update Roadmap UI and add texts

--- components/ui/roadmap.tsx
+++ components/ui/roadmap.tsx
@@ -3,24 +3,38 @@ export function Roadmap() {
     {
       quarter: 'Q1',
       goals: [
-        "New website launch",
-        "Strategic Partnership Announcement",
+        "Launch new SparkPoint website",
+        "Announce strategic partnerships",
         "Bridge SRK to Arbitrum",
-        "Launch SparkAgent AI Launchpad (No-Code AI Agent Creation)",
-        "Launch SparkPoint\'s first SparkAgent",
-        "SparkPoint Blockchain testnet launch"
+        "Launch SparkAgent AI Launchpad",
+        "Introduce SparkPoint's first SparkAgent"
       ]
     },
     {
       quarter: 'Q2',
       goals: [
         "Launch Trading AI Agent",
-        "SparkAgent AI Launchpad Improvements",
-        "SparkPoint Blockchain mainnet launch"
+        "Upgrade SparkAgent AI Launchpad new features",
+        "Deploy SparkPoint Blockchain mainnet",
+        "Expand SparkPoint's AI ecosystem"
       ]
     },
-    { quarter: "Q3" },
-    { quarter: "Q4" },
+    { quarter: "Q3",
+      goals: [
+        "Launch AI Agents Marketplace",
+        "Introduce Fully Autonomous SparkAgent",
+        "Integrate SRK as native token on SparkPoint Blockchain",
+        "Enable tradable AI agents with on-chain ownership"
+      ]
+    },
+    { quarter: "Q4",
+      goals: [
+        "Introduce Decentralized Agent-Based Blockchain Governance",
+        "Launch AI-powered DAO for SparkPoint",
+        "SparkPoint Blockchain cross-chain integrations",
+        "Next-gen SparkAgents for business growth automation"
+      ]
+    }
   ]
   return (
     <section className="px-6 py-10 md:px-20 bg-gradient-to-br from-[#3C8AC8] to-[#D8DDC7]">
@@ -46,9 +60,12 @@ function Card({
   return (
     <div className="flex flex-col bg-white p-6 gap-4 rounded-3xl hover:cursor-pointer h-full active:drop-shadow-none transition-all duration-200 hover:translate-y-[-0.25rem] hover:translate-x-[0.25rem] hover:shadow-[-0.25rem_0.25rem_#000] active:translate-x-0 active:translate-y-0 active:shadow-none">
       <h1 className="text-4xl md:text-5xl lg:text-7xl font-bold ">{title}</h1>
-      <ul className="list-disc pl-4">
+      <ul className="list-disc pl-4 space-y-8">
         {goals.map((goal, index) => (
-          <li className="text-lg" key={index}>{goal}</li>
+          <li className="text-base relative flex items-start custom-bullet" key={index}>
+            <span className="bullet"></span>
+            <span className="ml-2">{goal}</span>
+          </li>
         ))}
       </ul>
     </div>
