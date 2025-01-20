9fe08aa09997f527601a107b0fbe580c59338900
Bernard Historillo <bernardhistorillo1@gmail.com>
2025-01-20 18:20:45 +0800

    Update loading of dark mode settings in localstorage

--- components/layout/light-dark-toggle.tsx
+++ components/layout/light-dark-toggle.tsx
@@ -1,39 +1,51 @@
 'use client';
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
 
 const LightDarkToggle = () => {
-  // Persist dark mode state in localStorage
-  useEffect(() => {
-    const isDarkMode = localStorage.getItem("dark-mode") === "true";
-    if (isDarkMode) {
-      document.body.classList.add("dark-mode");
-    }
-  }, []);
+    // State to track dark mode
+    const [isDarkMode, setIsDarkMode] = useState(false);
 
-  const toggleDarkMode = () => {
-    const isDarkMode = document.body.classList.toggle("dark-mode");
-    localStorage.setItem("dark-mode", isDarkMode.toString());
-  };
+    // Load the initial state from localStorage when the component mounts
+    useEffect(() => {
+        const darkModeFromStorage = localStorage.getItem("dark-mode") === "true";
+        setIsDarkMode(darkModeFromStorage);
+        if (darkModeFromStorage) {
+            document.body.classList.add("dark-mode");
+        } else {
+            document.body.classList.remove("dark-mode");
+        }
+    }, []);
 
-  return (
-      <div className="fixed bottom-[70px] left-[10px] z-[10]" id="light-dark-toggle-container">
-        <input
-            type="checkbox"
-            className="checkbox"
-            id="checkbox"
-            onChange={toggleDarkMode}
-            defaultChecked={localStorage.getItem("dark-mode") === "true"}
-        />
-        <label htmlFor="checkbox" className="checkbox-label">
-            <FontAwesomeIcon icon={faMoon} />
-            <FontAwesomeIcon icon={faSun} />
-          <span className="ball"></span>
-        </label>
-      </div>
-  );
+    const toggleDarkMode = () => {
+        const newDarkModeState = !isDarkMode;
+        setIsDarkMode(newDarkModeState);
+        localStorage.setItem("dark-mode", newDarkModeState.toString());
+        if (newDarkModeState) {
+            document.body.classList.add("dark-mode");
+        } else {
+            document.body.classList.remove("dark-mode");
+        }
+    };
+
+    return (
+        <div className="fixed bottom-[70px] left-[10px] z-[10]" id="light-dark-toggle-container">
+            <input
+                type="checkbox"
+                className="checkbox"
+                id="checkbox"
+                onChange={toggleDarkMode}
+                checked={isDarkMode}
+            />
+            <label htmlFor="checkbox" className="checkbox-label">
+                <FontAwesomeIcon icon={faMoon} />
+                <FontAwesomeIcon icon={faSun} />
+                <span className="ball"></span>
+            </label>
+        </div>
+    );
 };
 
 export default LightDarkToggle;
