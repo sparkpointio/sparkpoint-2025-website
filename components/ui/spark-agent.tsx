17a890565ec795821ef3ef33e76f992c7a752e53
Harvey Javier <harveyjavier17@gmail.com>
2025-03-01 08:35:02 +0800

    Update link to mainnet

--- components/ui/spark-agent.tsx
+++ components/ui/spark-agent.tsx
@@ -83,9 +83,9 @@ const SparkAgent = () => {
               size: "2xl",
               className: 'sm:w-auto mt-8 mx-auto flex justify-center w-64 active:drop-shadow-none py-3 border border-black transition-all duration-200 cursor-pointer hover:-translate-y-[0.25rem] hover:translate-x-[-0.25rem] hover:text-[#000] hover:shadow-[0.25rem_0.25rem_#000] active:translate-x-0 active:translate-y-0 active:shadow-none button-1'
             })}
-            onClick={() => window.open('https://test.sparkpoint.io', '_blank')}
+            onClick={() => window.open('https://app.sparkpoint.io', '_blank')}
           >
-            Try our Testnet!
+            Create Your Agent Now!
           </button>
         </Waitlist>
       </div>
